a0f21ac2c24807d6faccc65c5e5b79e9816c637a
sebas7ian1802 <mvsebastian1802@gmail.com>
2025-11-25 10:34:03 -0500

    SEMANA15

---
+++ S15_FUNAL_TEORIA_ARREGLOS_BIDIMENSIONALES_PY/S15_FUNAL_TEORIA_ARREGLOS_BIDIMENSIONALES_PY.py
@@ -0,0 +1,42 @@
+import numpy as np
+n=int(input("Digite el numero de orden de la matriz cuadrada: "))
+#matriz=np.random.randint(0, 21, (n,n)) 
+#matriz=np.random.randint(100, 1000, (n,n))
+matriz=np.random.uniform(10, 100,(n,n))  
+matriz2=np.round(matriz,2)           
+print(matriz)
+print()
+print(matriz2)
+
+#falta hacer ahora la multiplicacion de matrices
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
